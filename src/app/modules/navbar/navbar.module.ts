ba9f83ba4569cb067617809beee6997b11929b1e
Muaaz Ali <voltmeter512@gmail.com>
2020-07-25 01:17:36 +0500

    Navbar UI is complete; added the title, and a search bar to the right.

--- src/app/modules/navbar/navbar.module.ts
+++ src/app/modules/navbar/navbar.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { NavItemComponent } from './components/nav-item/nav-item.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { NavSearchComponent } from './components/nav-search/nav-search.component';
 
 @NgModule({
-    declarations: [NavbarComponent, NavItemComponent],
-    imports: [CommonModule, MatIconModule],
+    declarations: [NavbarComponent, NavItemComponent, NavSearchComponent],
+    imports: [CommonModule, MatIconModule, MatInputModule],
     exports: [NavbarComponent],
 })
 export class NavbarModule {}
