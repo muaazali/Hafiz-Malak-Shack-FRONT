c62fa7085e99cec9aff9b69b9fabd4f80075d8c1
Muaaz Ali <voltmeter512@gmail.com>
2020-07-25 02:44:58 +0500

    Home Componenet's layout defined w/o using any additional components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './modules/user/user.component';
+import { HomeComponent } from './modules/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UserComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './modules/navbar/navbar.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserModule } from './modules/user/user.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, NavbarModule, BrowserAnimationsModule],
+    imports: [BrowserModule, AppRoutingModule, NavbarModule, BrowserAnimationsModule, UserModule, HomeModule],
     providers: [],
     bootstrap: [AppComponent],
 })
